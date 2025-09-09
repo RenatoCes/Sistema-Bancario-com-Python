c43f77fdf9ce15751b1685e2ab0627ce5e4b3b8c
RenatoCes <Renatojudev@gmail.com>
2025-09-08 23:59:27 -0300

    Login, registro e saldo individual, realizados.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -13,10 +13,11 @@ telas = ["""
 -->"""]
 tela = 0
 nome = ""
-saldo = 0.0
-limite = 0.0
-extrato_bancario = [[[],[],[],[]]]
+gmail = ""
+extrato_bancario = [[[],[],[],[],]] #valor, data, hora, bool
+saldo_e_quantia_de_saques = [[[0.0],[0]]]
 email_senha_usuario = [["python@gmail.com"],["12345678"],["Python"]]
+index = 0
 sistema = True
 quantia_de_saques_realizados_no_dia = 0
 #TODO fazer uma lista de depósitos diários, separar email_senha_usuario, adicionar lista para cpf ou substiituir email por cpf, saldo individual
@@ -29,29 +30,27 @@ def validador_de_valor_de_entrada(valor):
     else:
         return 0.0
     
-def deposito(valor, index_email):
-    print(index_email)
-    if valor > 0 and extrato_bancario[index_email][1].count(datetime.now().strftime("%d/%m/%Y")) < 10:
+def deposito(valor, index):
+    if valor > 0 and extrato_bancario[index][1].count(datetime.now().strftime("%d/%m/%Y")) < 10:
         print(f"Deposio de R${valor:.2f} realizado.")
-        acrescentar_no_extrato(valor, True, index_email)
+        acrescentar_no_extrato(valor, True, index)
         return True, valor
-    elif valor > 0 and extrato_bancario[index_email][1].count(datetime.now().strftime("%d/%m/%Y")) >= 10:
+    elif valor > 0 and extrato_bancario[index][1].count(datetime.now().strftime("%d/%m/%Y")) >= 10:
         print("Deposito negado, limite de transações realizadas.")
         return False, 0
     else:
         print("Deposito negado, tente novamente.")
         return False, 0
 
-def saque(valor, saldo, quantia_de_saques_realizados_no_dia, index_email):
-    print(index_email)
-    transacoes_do_dia = extrato_bancario[index_email][1].count(datetime.now().strftime("%d/%m/%Y"))
+def saque(valor, saldo, quantia_de_saques_realizados_no_dia, index):
+    transacoes_do_dia = extrato_bancario[index][1].count(datetime.now().strftime("%d/%m/%Y"))
     saque_valido = valor <= saldo and valor > 0 and valor <= 500 and quantia_de_saques_realizados_no_dia < 3 and transacoes_do_dia < 10
     saque_limite_transacao = valor > 0 and valor > 500 and quantia_de_saques_realizados_no_dia < 3 and quantia_de_saques_realizados_no_dia  < 10
     saque_limite_monetario = valor > 0 and valor > 500 and quantia_de_saques_realizados_no_dia < 3
     saque_limite_diario = valor > 0 and quantia_de_saques_realizados_no_dia >=3
     if saque_valido:
         print(f"Saque de R${valor:.2f} realizado.")
-        acrescentar_no_extrato(valor, False, index_email)
+        acrescentar_no_extrato(valor, False, index)
         quantia_de_saques_realizados_no_dia
         return True, valor
     elif saque_limite_transacao:
@@ -67,20 +66,19 @@ def saque(valor, saldo, quantia_de_saques_realizados_no_dia, index_email):
         print("Saque negado, tente novamente.")
         return False, 0
 
-def acrescentar_no_extrato(valor, valor_positivo_negativo, index_email):
+def acrescentar_no_extrato(valor, valor_positivo_negativo, index):
     data_tempo = datetime.now()
-    extrato_bancario[index_email][0].append(valor)
-    extrato_bancario[index_email][1].append(data_tempo.strftime("%d/%m/%Y"))
-    extrato_bancario[index_email][2].append(data_tempo.strftime("%H:%M:%S"))
-    extrato_bancario[index_email][3].append(valor_positivo_negativo)
+    extrato_bancario[index][0].append(valor)
+    extrato_bancario[index][1].append(data_tempo.strftime("%d/%m/%Y"))
+    extrato_bancario[index][2].append(data_tempo.strftime("%H:%M:%S"))
+    extrato_bancario[index][3].append(valor_positivo_negativo)
 
-def extrato(index_email):
-    print(index_email)
-    for x in range(len(extrato_bancario[index_email][0])):
-            if extrato_bancario[index_email][3][x] == True:
-                print(f"Deposito de R${extrato_bancario[index_email][0][x]:.2f} data: {extrato_bancario[index_email][1][x]} horario: {extrato_bancario[index_email][2][x]}")
+def extrato(index, saldo):
+    for x in range(len(extrato_bancario[index][0])):
+            if extrato_bancario[index][3][x] == True:
+                print(f"Deposito de R${extrato_bancario[index][0][x]:.2f} data: {extrato_bancario[index][1][x]} horario: {extrato_bancario[index][2][x]}")
             else:
-                print(f"Saque de R${extrato_bancario[index_email][0][x]:.2f} data: {extrato_bancario[index_email][1][x]} horario: {extrato_bancario[index_email][2][x]}")       
+                print(f"Saque de R${extrato_bancario[index][0][x]:.2f} data: {extrato_bancario[index][1][x]} horario: {extrato_bancario[index][2][x]}")       
     print(f"saldo: R${saldo:.2f}")
 
 def validar_registro(email, senha):
@@ -115,6 +113,7 @@ while sistema == True:
                         if validar_login(email, senha) == True:
                             tela = 1
                             nome = email_senha_usuario[2][email_senha_usuario[0].index(email)]
+                            index = email_senha_usuario[0].index(email)
 
                     case "2":
                         nome = input("Nome:\n-->")
@@ -125,10 +124,13 @@ while sistema == True:
                             email_senha_usuario[1].append(senha)
                             email_senha_usuario[2].append(nome)
                             extrato_bancario.append([[],[],[],[]])
+                            saldo_e_quantia_de_saques.append([[0.0],[0]])
+                            index = email_senha_usuario[0].index(email)
                             tela = 1
                             print(email_senha_usuario, extrato_bancario)
 
                     case "3":
+                        print("Fim do Programa")
                         exit()
             else:
                 print("Opção invalida!")
@@ -141,14 +143,14 @@ while sistema == True:
                     case "1" :
                         resposta_do_deposito = deposito(validador_de_valor_de_entrada(float(input("Valor do Deposito\n-->R$"))), email_senha_usuario[0].index(email))
                         if resposta_do_deposito[0] == True:
-                            saldo += resposta_do_deposito[1]
+                            saldo_e_quantia_de_saques[index][0][0] += resposta_do_deposito[1]
                     case "2" : 
-                        resposta_do_saque = saque(validador_de_valor_de_entrada(float(input("Valor do Saque\n-->R$"))), saldo, quantia_de_saques_realizados_no_dia, email_senha_usuario[0].index(email))
+                        resposta_do_saque = saque(validador_de_valor_de_entrada(float(input("Valor do Saque\n-->R$"))), saldo_e_quantia_de_saques[index][0][0], saldo_e_quantia_de_saques[index][1][0], email_senha_usuario[0].index(email))
                         if resposta_do_saque[0] == True:
-                            saldo -= resposta_do_saque[1]
-                            quantia_de_saques_realizados_no_dia += 1
+                            saldo_e_quantia_de_saques[index][0][0] -= resposta_do_saque[1]
+                            saldo_e_quantia_de_saques[index][1][0] += 1
                     case "3" :
-                        extrato(email_senha_usuario[0].index(email))
+                        extrato(email_senha_usuario[0].index(email), saldo_e_quantia_de_saques[index][0][0])
                     case "4" :
                         tela = 0
 
